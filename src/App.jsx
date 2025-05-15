e598df6e13eaa827308599a5cc096deaa1156830
최우석 <56614719+chldntjr1321@users.noreply.github.com>
2025-05-15 21:56:02 +0900

    성능개선 - lazy import, Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import './App.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import data from './data.jsx';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import Detail from './routes/Detail.jsx';
-import Cart from './routes/Cart.jsx';
+// import Detail from './routes/Detail.jsx';
+// import Cart from './routes/Cart.jsx';
+const Detail = lazy(() => import('./routes/Detail.jsx'));
+const Cart = lazy(() => import('./routes/Cart.jsx'));
+
 import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -21,9 +25,21 @@ function App() {
     } else localStorage.setItem('watched', JSON.stringify([]));
   }, []);
 
+  let result = useQuery({
+    queryKey: ['작명'],
+    queryFn: () => {
+      return axios
+        .get('https://codingapple1.github.io/userdata.json')
+        .then((a) => {
+          console.log('요청됨');
+          return a.data;
+        });
+    },
+  });
+
   return (
     <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/">Woo's Shop</Navbar.Brand>
           <Nav className="me-auto">
@@ -49,6 +65,11 @@ function App() {
               Cart
             </Nav.Link>
           </Nav>
+          <Nav className="ms-auto">
+            {result.isLoading && '로딩중'}
+            {result.error && '에러남'}
+            {result.data && result.data.name}
+          </Nav>
         </Container>
       </Navbar>
 
@@ -56,75 +77,77 @@ function App() {
       {/* <Link to="/">홈</Link>
       <Link to="/detail">상세페이지</Link> */}
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="main-bg"></div>
+      <Suspense fallback={<div>로딩중임</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="main-bg"></div>
 
-              <Container>
-                <Row>
-                  {shoes.map(function (a, i) {
-                    return (
-                      <Goods
-                        shoes={shoes[i]}
-                        i={i}
-                        navigate={navigate}
-                        key={i}
-                      />
-                    );
-                  })}
-                </Row>
-              </Container>
-              {loadingUI == true ? <Loading /> : null}
-              {morebtn == true ? (
-                <button
-                  onClick={() => {
-                    if (btncount >= 2) {
-                      alert('상품이 더이상 없습니다!');
-                      setMoreBtn(false);
-                      return;
-                    }
-                    let count = btncount + 1;
-                    setBtncount(count);
-                    setLoadingUI(true);
-                    axios
-                      .get(
-                        'https://codingapple1.github.io/shop/data' +
-                          (count + 1) +
-                          '.json'
-                      )
-                      .then((result) => {
-                        let newShoes = [...shoes, ...result.data];
-                        setShoes(newShoes);
-                        setLoadingUI(false);
-                      })
-                      .catch(() => {
-                        setLoadingUI(false);
-                        console.log('실패함');
-                      });
-                  }}
-                >
-                  더보기
-                </button>
-              ) : null}
-            </>
-          }
-        />
+                <Container>
+                  <Row>
+                    {shoes.map(function (a, i) {
+                      return (
+                        <Goods
+                          shoes={shoes[i]}
+                          i={i}
+                          navigate={navigate}
+                          key={i}
+                        />
+                      );
+                    })}
+                  </Row>
+                </Container>
+                {loadingUI == true ? <Loading /> : null}
+                {morebtn == true ? (
+                  <button
+                    onClick={() => {
+                      if (btncount >= 2) {
+                        alert('상품이 더이상 없습니다!');
+                        setMoreBtn(false);
+                        return;
+                      }
+                      let count = btncount + 1;
+                      setBtncount(count);
+                      setLoadingUI(true);
+                      axios
+                        .get(
+                          'https://codingapple1.github.io/shop/data' +
+                            (count + 1) +
+                            '.json'
+                        )
+                        .then((result) => {
+                          let newShoes = [...shoes, ...result.data];
+                          setShoes(newShoes);
+                          setLoadingUI(false);
+                        })
+                        .catch(() => {
+                          setLoadingUI(false);
+                          console.log('실패함');
+                        });
+                    }}
+                  >
+                    더보기
+                  </button>
+                ) : null}
+              </>
+            }
+          />
 
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<div>없는 페이지요</div>} />
-        <Route path="/about" element={<About />}>
-          <Route path="member" element={<div>멤버임</div>} />
-          <Route path="location" element={<div>로케이션임</div>} />
-        </Route>
-        <Route path="/event" element={<Event />}>
-          <Route path="one" element={<div>첫 주문 시 양배추즙 서비스</div>} />
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
-      </Routes>
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<div>없는 페이지요</div>} />
+          <Route path="/about" element={<About />}>
+            <Route path="member" element={<div>멤버임</div>} />
+            <Route path="location" element={<div>로케이션임</div>} />
+          </Route>
+          <Route path="/event" element={<Event />}>
+            <Route path="one" element={<div>첫 주문 시 양배추즙 서비스</div>} />
+            <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
